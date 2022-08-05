d93992ed6759854a8806ae688a43e9a4c8732846
Dennis Schaller <dennis.schaller@klarna.com>
2022-08-05 12:40:56 +0200

    add split encoder

--- app/include/zmk/events/sensor_event.h
+++ app/include/zmk/events/sensor_event.h
@@ -6,13 +6,15 @@
 
 #pragma once
 
+#include <drivers/sensor.h>
 #include <zephyr.h>
 #include <zmk/event_manager.h>
 #include <device.h>
 struct zmk_sensor_event {
     uint8_t sensor_number;
-    const struct device *sensor;
+    struct sensor_value value;
     int64_t timestamp;
 };
 
 ZMK_EVENT_DECLARE(zmk_sensor_event);
+q
