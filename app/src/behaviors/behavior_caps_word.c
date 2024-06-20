1548e4a84e9f53f957a7c52bbedd621b3eed6c71
Dennis Schaller <dennis.schaller@klarna.com>
2024-06-20 11:18:54 +0200

    add caps_word behavior

--- app/src/behaviors/behavior_caps_word.c
+++ app/src/behaviors/behavior_caps_word.c
@@ -20,6 +20,10 @@
 #include <zmk/hid.h>
 #include <zmk/keymap.h>
 
+#include <zmk/caps_word.h> 
+
+bool last_state_of_caps_word = false;
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
@@ -43,12 +47,16 @@ struct behavior_caps_word_data {
 
 static void activate_caps_word(const struct device *dev) {
     struct behavior_caps_word_data *data = dev->data;
+ 
+	last_state_of_caps_word = true;
 
     data->active = true;
 }
 
 static void deactivate_caps_word(const struct device *dev) {
     struct behavior_caps_word_data *data = dev->data;
+  
+	last_state_of_caps_word = false;
 
     data->active = false;
 }
